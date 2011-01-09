b07b69b6d6e75d12a5bb5ba93b6c3a81404636d1
Moutarde <nicolask77@free.fr>
2011-01-09 18:16:50 +0100

    First commit containing the Enemy files

---
+++ Enemy/enemy.h
@@ -0,0 +1,13 @@
+	#include <stdio.h>
+	#include "typeEn.h"
+
+typedef struct {
+	int x;
+	int y;
+	int life;
+	int speed;
+	int isPoisoned;
+	TypeEn type;
+} Enemy;
+
+Enemy *createEnemy(int x, int y, TypeEn type);
---
+++ Enemy/typeEn.h
@@ -0,0 +1,15 @@
+	#include <stdio.h>
+	#include <SDL.h>
+
+typedef struct {
+	int maxLife;
+	SDL_Surface* image;
+	int normalSpeed;
+	int canFly;
+	int canBeSlowed;
+	int canBePoisoned;
+	int invisible;
+	int armor;
+} TypeEn;
+
+TypeEn *createTypeEn(int maxLife, SDL_Surface* image, int normalSpeed, int canFly, int canBeSlowed, int canBePoisoned, int invisible, int armor);
