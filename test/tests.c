631cf6e7f7dffaee91826d914e4483f31447ed6e
NixWord <lewinp@esiee.fr>
2011-02-03 22:21:01 +0100

    Creating namespace for tests

--- test/tests.c
+++ test/tests.c
@@ -9,10 +9,26 @@
 */
 #include <CUnit/Console.h>
 
+#include "test/enemy/test_enemy.h"
+
 int main()
 {
+	CU_pSuite enemySuite = NULL;
+
 	if (CUE_SUCCESS != CU_initialize_registry())
 		return CU_get_error();
+
+	enemySuite = CU_add_suite("Suite_1", initEnemySuite, cleanEnemySuite);
+	if (NULL == enemySuite) {
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
+
+	if (NULL == CU_add_test(enemySuite, "Create Enemy", testCreateEnemy))
+	{
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
 
 
 	CU_console_run_tests();
